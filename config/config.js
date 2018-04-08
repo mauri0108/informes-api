327cf6de0a022f03cebf5e1ac5318e9cbf487563
mjlucero <mauricio.lucero2014@gmail.com>
2018-04-08 16:43:35 -0300

    Delete config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports.SEED = "dominaecosystem";
-module.exports.EMAIL = "dominaeco.system@gmail.com";
-module.exports.PASS = "MV9/bRz("
-module.exports.URL_CLIENTE = "http://localhost:4200/#/nueva-pass?token="
